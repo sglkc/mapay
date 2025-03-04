bc504a50686d2a0ab7b2ef8be1909fc98e434e74
Seya <31957516+sglkc@users.noreply.github.com>
2025-03-04 20:16:45 +0700

    refactor: move preact app to client, api to server
    build(pnpm): add kysely

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import unocss from 'unocss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
+  root: resolve(__dirname, 'client'),
+  build: {
+    outDir: '../dist/client',
+    emptyOutDir: true,
+  },
+  plugins: [
     preact(),
     unocss(),
   ],
