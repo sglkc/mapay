fbfad3fe165de050f539c846a2ef65cc642388ab
Seya <31957516+sglkc@users.noreply.github.com>
2025-03-05 21:47:56 +0700

    GANTI JADI LARAVEL + FILAMENT PHP SAJA!!!

--- server/vite.ts
+++
@@ -1,6 +0,0 @@
-import ViteExpress from 'vite-express'
-import app from './api'
-
-const PORT = Number(process.env.PORT) || 5173
-
-ViteExpress.listen(app, PORT, () => console.log(`Server is listening on ${PORT}`))
