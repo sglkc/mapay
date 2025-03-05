4c1e1fc38ad59d30c7e883897eb1eb2c26e035e3
Seya <31957516+sglkc@users.noreply.github.com>
2025-03-05 14:06:59 +0700

    build(pnpm): add brcypt

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,12 +1,29 @@
 import { Router } from 'express'
+import bcrypt from 'bcrypt'
 import { NewUser } from '~/types/database'
 import { db } from '../database'
 
 const UserRouter = Router()
 
+UserRouter.get<{ id: string }>('/user/:id', async (req, res) => {
+  const user = await db.selectFrom('user')
+    .where('id', '=', Number(req.params.id))
+    .selectAll()
+    .executeTakeFirstOrThrow()
+
+  res.json({ user })
+})
+
 UserRouter.post<{}, {}, NewUser>('/user', async (req, res) => {
+  const password = bcrypt.hashSync(req.body.password, 4)
+
+  const values: NewUser = {
+    username: req.body.username,
+    password,
+  }
+
   const user = await db.insertInto('user')
-    .values({ username: 'gaming' })
+    .values(values)
     .returningAll()
     .executeTakeFirstOrThrow()
 
--- types/database.ts
+++ types/database.ts
@@ -13,6 +13,7 @@ export interface Database {
 export interface UserTable {
   id: Generated<number>
   username: string
+  password: string
 }
 
 export type User = Selectable<UserTable>
